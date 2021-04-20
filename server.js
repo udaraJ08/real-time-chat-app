3a3cd7fd35622b367fda5111b8c5e121c8064751
USER <udaraj08@gmail.com>
2021-04-20 16:23:39 +0530

    grp chat final proto

--- server.js
+++ server.js
@@ -17,6 +17,18 @@ io.on("connection", (socket) => {
   console.log("Socket is established !!!");
 
   socket.on("chat", (data) => {
-    io.sockets.emit("msg", data);
+    io.sockets.emit("msg", `${data.name}: ${data.message}`);
+  });
+
+  socket.on("typing", (data) => {
+    socket.broadcast.emit("status", `${data.name} ${data.status}`);
+  });
+
+  socket.on("join", (data) => {
+    socket.broadcast.emit("join", `${data.name} joined`);
+  });
+
+  socket.on("disconnect", function () {
+    io.sockets.emit("disconnect", "left");
   });
 });
--- server/public/scripts/chat.js
+++ server/public/scripts/chat.js
@@ -1,24 +1,70 @@
-const socket = io.connect("https://big-z-chat.herokuapp.com/");
+const socket = io.connect("http://localhost:3000/");
 const audio = new Audio("../assets/audio/button.mp3");
 
+let output = "";
+
+let nickName = "Anonymous123";
+
+// window.onbeforeunload = function () {
+//   return "Are you really want to perform the action?";
+// };
+
+$("#nkSubmitBtn").on("click", () => {
+  const name = $("#nickName").val();
+  if (name.length > 2 && name.length < 10) {
+    nickName = name;
+    socket.emit("join", { name: nickName });
+    $("#nickName-container").slideUp();
+  }
+});
+
 $("#btn").on("click", () => {
-  audio.play();
+  bottomScroller();
   const msgData = $("#msg").val();
-  socket.emit("chat", msgData);
+  socket.emit("chat", { name: nickName, message: msgData });
   $("#msg").val("");
   $("#msg").focus();
+  socket.emit("typing", { name: "", status: "" });
 });
 
 $("#msg").keydown((key) => {
   if (key.key == "Enter") {
-    audio.play();
+    bottomScroller();
     const msgData = $("#msg").val();
-    socket.emit("chat", msgData);
+    socket.emit("chat", { name: nickName, message: msgData });
     $("#msg").val("");
     $("#msg").focus();
+    socket.emit("typing", { name: "", status: "" });
+  } else {
+    socket.emit("typing", { name: nickName, status: "is typing..." });
   }
 });
 
 socket.on("msg", (data) => {
-  $(".content").append(`<h1>${data}</h1>`);
+  audio.play();
+  output += `<h1>${data}</h1>`;
+  $("#output").html(output);
 });
+
+socket.on("status", (data) => {
+  $("#status").html(`<h4 style="color: #58B19F;"><em>${data}</em></h4>`);
+});
+
+socket.on("join", (data) => {
+  output += `<h1 style="color: #fbc531;">${data}</h1>`;
+  $("#output").html(output);
+});
+
+socket.on("disconnect", (data) => {
+  output += `<h1 style="color: #eb4d4b;">someone left</h1>`;
+  $("#output").html(output);
+});
+
+function bottomScroller() {
+  $(".content").animate(
+    {
+      scrollTop: $(".content")[0].scrollHeight,
+    },
+    "slow"
+  );
+}
