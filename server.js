71b619322ef488b3d6ccc31c0b30eec576f7e79b
USER <udaraj08@gmail.com>
2021-04-20 12:03:29 +0530

    ready to upload

--- server.js
+++ server.js
@@ -8,10 +8,8 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.log("Server is listening !!!");
 });
 
-const x = 0;
 const staticPath = "./server/public";
 app.use(express.static(staticPath));
-console.log(staticPath);
 app.use(router);
 const io = socket(server);
 
